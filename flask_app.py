0d22a60bfbf8326b4cdd62d66221b7ea5362eab9
Kusuma Kalyan <49522429+KusumaKalyan@users.noreply.github.com>
2021-11-14 10:55:07 -0800

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,59 @@
+from flask import Flask, send_file, request
+from datetime import datetime
+import pytz
+from cryptography.fernet import Fernet
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'kusumakalyan.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = 'kusumakalyan'
+app.config['MYSQL_PASSWORD'] = '8096311455mysql'
+app.config['MYSQL_DB'] = 'kusumakalyan$pccontroller'
+
+mysql = MySQL(app)
+
+@app.route('/download')
+def downloadFile ():
+    path = "./Credential Vault.apk"
+    return send_file(path, as_attachment=True)
+
+@app.route('/version')
+def version ():
+    return '3'
+
+@app.route('/getonoffvalue')
+def getOnOffValue():
+    cur = mysql.connection.cursor()
+    cur.execute("select value from pc_controller;")
+    z = cur.fetchall()[0][0]
+    mysql.connection.commit()
+    cur.close()
+    return str(z)
+
+@app.route('/setonvalue', methods=['POST'])
+def setOnValue():
+    f = Fernet(b'DAvY1K4-Mhvqb3iNS9qJhYni99Olz8Ksty3ZFKGrir4=')
+    raw_time = str(datetime.now(pytz.timezone('Asia/Kolkata'))).split(' ')[1].split(':')
+    if(f.decrypt(bytes(request.args.get('code'), 'utf-8')).decode('utf-8') == raw_time[0]+raw_time[1]):
+        cur = mysql.connection.cursor()
+        cur.execute("update pc_controller set value=1;")
+        mysql.connection.commit()
+        cur.close()
+        return "successfull"
+    else:
+        return "failed"
+
+@app.route('/setoffvalue', methods=['POST'])
+def setOffValue():
+    f = Fernet(b'DAvY1K4-Mhvqb3iNS9qJhYni99Olz8Ksty3ZFKGrir4=')
+    raw_time = str(datetime.now(pytz.timezone('Asia/Kolkata'))).split(' ')[1].split(':')
+    if(f.decrypt(bytes(request.args.get('code'), 'utf-8')).decode('utf-8') == raw_time[0]+raw_time[1]):
+        cur = mysql.connection.cursor()
+        cur.execute("update pc_controller set value=0;")
+        mysql.connection.commit()
+        return "successfull"
+    else:
+        return "failed"
+    cur.close()
+    return "successfull"
